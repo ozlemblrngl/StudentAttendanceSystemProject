b15a0d5ae9f50cb2fd0e1f250e6417710c863ab5
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-13 19:35:02 +0300

    update button codes added

--- StudentAttendanceProject/StudentAttendanceProject/Form1.vb
+++ StudentAttendanceProject/StudentAttendanceProject/Form1.vb
@@ -7,7 +7,7 @@ Public Class StudentAttendance
         Dim rollNo As String = TxtRollNo.Text
         Dim status As String = TxtStatus.Text
 
-        Dim query As String = ("insert into newtab values (@studentID,@name,@rollNo,@status")
+        Dim query As String = ("insert into newtab values (@studentID,@name,@rollNo,@status)")
         Using con As SqlConnection = New SqlConnection("Data Source= Data Source=DESKTOP-N9UK4PN;Initial Catalog=StudentAttendanceDB;Integrated Security=True;Trust Server Certificate=True")
             Using cnn As SqlCommand = New SqlCommand(query, con)
                 cnn.Parameters.AddWithValue("@StudentID", studentId)
@@ -40,7 +40,43 @@ Public Class StudentAttendance
                     End Using
                 End Using
             End Using
+        End Using
+    End Sub
 
+    Private Sub BtnUpdate_Click(sender As Object, e As EventArgs) Handles BtnUpdate.Click
+        Dim studentId As Integer = TxtStudentId.Text
+        Dim name As String = TxtStudentName.Text
+        Dim rollNo As String = TxtRollNo.Text
+        Dim status As String = TxtStatus.Text
+
+        Dim query As String = ("update newtab set name=@name,rollNo= @rollNo,status=@status where studentid = @studentid")
+        Using con As SqlConnection = New SqlConnection("Data Source=Data Source=DESKTOP-N9UK4PN;Initial Catalog=StudentAttendanceDB;Integrated Security=True;Trust Server Certificate=True ")
+            con.Open()
+            Using cnn As SqlCommand = New SqlCommand(query, con)
+                cnn.Parameters.AddWithValue("@StudentID", studentId)
+                cnn.Parameters.AddWithValue("@Name", name)
+                cnn.Parameters.AddWithValue("@RollNo", rollNo)
+                cnn.Parameters.AddWithValue("@Status", status)
+                cnn.ExecuteNonQuery()
+                con.Close()
+            End Using
+
+            MessageBox.Show("Record Updated Successfully")
+        End Using
+
+    End Sub
+
+    Private Sub StudentAttendance_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim query As String = ("select * from newtab")
+        Using con As SqlConnection = New SqlConnection("Data Source=Data Source=DESKTOP-N9UK4PN;Initial Catalog=StudentAttendanceDB;Integrated Security=True;Trust Server Certificate=True ")
+            Using cnn As SqlCommand = New SqlCommand(query, con)
+                Using da As New SqlDataAdapter(cnn)
+                    Using table As New DataTable()
+                        da.Fill(table)
+                        DataGridView1.DataSource = table
+                    End Using
+                End Using
+            End Using
         End Using
     End Sub
 End Class
