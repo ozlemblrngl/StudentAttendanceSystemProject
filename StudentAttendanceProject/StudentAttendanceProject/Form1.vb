33e9bc8c3c5aeeeba032505a026f1c8651ed016d
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-13 19:42:21 +0300

    delete button codes added

--- StudentAttendanceProject/StudentAttendanceProject/Form1.vb
+++ StudentAttendanceProject/StudentAttendanceProject/Form1.vb
@@ -2,7 +2,7 @@
 
 Public Class StudentAttendance
     Private Sub BtnSave_Click(sender As Object, e As EventArgs) Handles BtnSave.Click
-        Dim studentId As Integer = TxtStudentId.Text
+        Dim id As Integer = TxtStudentId.Text
         Dim name As String = TxtStudentName.Text
         Dim rollNo As String = TxtRollNo.Text
         Dim status As String = TxtStatus.Text
@@ -10,7 +10,7 @@ Public Class StudentAttendance
         Dim query As String = ("insert into newtab values (@studentID,@name,@rollNo,@status)")
         Using con As SqlConnection = New SqlConnection("Data Source= Data Source=DESKTOP-N9UK4PN;Initial Catalog=StudentAttendanceDB;Integrated Security=True;Trust Server Certificate=True")
             Using cnn As SqlCommand = New SqlCommand(query, con)
-                cnn.Parameters.AddWithValue("@StudentID", studentId)
+                cnn.Parameters.AddWithValue("@Id", id)
                 cnn.Parameters.AddWithValue("@Name", name)
                 cnn.Parameters.AddWithValue("@RollNo", rollNo)
                 cnn.Parameters.AddWithValue("@Status", status)
@@ -44,24 +44,25 @@ Public Class StudentAttendance
     End Sub
 
     Private Sub BtnUpdate_Click(sender As Object, e As EventArgs) Handles BtnUpdate.Click
-        Dim studentId As Integer = TxtStudentId.Text
+        Dim id As Integer = TxtStudentId.Text
         Dim name As String = TxtStudentName.Text
         Dim rollNo As String = TxtRollNo.Text
         Dim status As String = TxtStatus.Text
 
-        Dim query As String = ("update newtab set name=@name,rollNo= @rollNo,status=@status where studentid = @studentid")
+        Dim query As String = ("update newtab set name=@name,rollNo= @rollNo,status=@status where id = @id")
         Using con As SqlConnection = New SqlConnection("Data Source=Data Source=DESKTOP-N9UK4PN;Initial Catalog=StudentAttendanceDB;Integrated Security=True;Trust Server Certificate=True ")
             con.Open()
+
             Using cnn As SqlCommand = New SqlCommand(query, con)
-                cnn.Parameters.AddWithValue("@StudentID", studentId)
+                cnn.Parameters.AddWithValue("@Id", id)
                 cnn.Parameters.AddWithValue("@Name", name)
                 cnn.Parameters.AddWithValue("@RollNo", rollNo)
                 cnn.Parameters.AddWithValue("@Status", status)
                 cnn.ExecuteNonQuery()
                 con.Close()
+                MessageBox.Show("Record Updated Successfully")
             End Using
 
-            MessageBox.Show("Record Updated Successfully")
         End Using
 
     End Sub
@@ -78,5 +79,24 @@ Public Class StudentAttendance
                 End Using
             End Using
         End Using
+    End Sub
+
+    Private Sub BtnDelete_Click(sender As Object, e As EventArgs) Handles BtnDelete.Click
+        Dim Id As Integer = TxtStudentId.Text
+        Dim query As String = "delete newtab where id=@id"
+
+        Using con As SqlConnection = New SqlConnection("Data Source=Data Source=DESKTOP-N9UK4PN;Initial Catalog=StudentAttendanceDB;Integrated Security=True;Trust Server Certificate=True ")
+            con.Open()
+
+            Using cnn As SqlCommand = New SqlCommand(query, con)
+                cnn.Parameters.AddWithValue("@Id", Id)
+                cnn.ExecuteNonQuery()
+                con.Close()
+                MessageBox.Show("Record Deleted Successfully")
+
+            End Using
+
+        End Using
+
     End Sub
 End Class
